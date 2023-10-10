bf285a0765883d9a63e5d946bd72c2e3f8322976
DanielColucci <142242668+DanielColucci@users.noreply.github.com>
2023-10-10 10:38:38 -0600

    Borre texto

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Ya");
-        System.out.println("Hola");
-        System.out.println("Putos todos, menos yo");
-        System.out.println("Bromi");
+  
     }
 }
