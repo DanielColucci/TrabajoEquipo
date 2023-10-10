3379445dbfaa7a4d6e1280368a65ddd36a4a694d
DanielColucci <142242668+DanielColucci@users.noreply.github.com>
2023-10-10 10:36:41 -0600

    ya

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Ya");
         System.out.println("Hola");
         System.out.println("Putos todos, menos yo");
+        System.out.println("Bromi");
     }
 }
