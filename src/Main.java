bbf10a65ed563ebe58887d991e06b41b32869c07
Ivan SQ <isanchezq002@alumno.uaemex.mx>
2023-10-10 11:05:00 -0600

    listo gfe

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Ya");
         System.out.println("ptos");
+        System.out.println("Vamos a quedarnos?");
     }
 }
